74e2af25faa2a84e84988fef6e3e5a2eb1eb8d9b
UsmanAAV <usman.a.a.v@gmail.com>
2018-10-10 11:28:38 +0500

    made frame of index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+export const initPlayField = () => {
+
+};
+
+export const movePlayField = (playField) => {
+
+};
+
+export const addNumberToPlayField = (playField) => {
+
+};
+
+export const isWin = (playField) => {
+
+};
+
+export const isEnd = (playField) => {
+
+};
