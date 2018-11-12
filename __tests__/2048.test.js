e7da1dd251f4f375ce1b8e4caa25ca436ff993fb
UsmanAAV <usman.a.a.v@gmail.com>
2018-11-12 12:24:20 +0500

    added tests

--- __tests__/2048.test.js
+++ __tests__/2048.test.js
@@ -40,18 +40,69 @@ const playField1Down = [
   [2, 4, 8, 4],
 ];
 
-it('move left', () => {
+it('move left playField1', () => {
   expect(movePlayField(playField1, LEFT)).toEqual(playField1Left);
 });
 
-it('move up', () => {
+it('move up playField1', () => {
   expect(movePlayField(playField1, UP)).toEqual(playField1Up);
 });
 
-it('move right', () => {
+it('move right playField1', () => {
   expect(movePlayField(playField1, RIGHT)).toEqual(playField1Right);
 });
 
-it('move down', () => {
+it('move down playField1', () => {
   expect(movePlayField(playField1, DOWN)).toEqual(playField1Down);
+});
+
+const playField2 = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 2, 0],
+  [0, 0, 0, 0],
+]
+
+const playField2Left = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [2, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+
+const playField2Up = [
+  [0, 0, 2, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+
+const playField2Right = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 2],
+  [0, 0, 0, 0],
+];
+
+const playField2Down = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 2, 0],
+];
+
+it('move left playField2', () => {
+  expect(movePlayField(playField2, LEFT)).toEqual(playField2Left);
+});
+
+it('move up playField2', () => {
+  expect(movePlayField(playField2, UP)).toEqual(playField2Up);
+});
+
+it('move right playField2', () => {
+  expect(movePlayField(playField2, RIGHT)).toEqual(playField2Right);
+});
+
+it('move down playField2', () => {
+  expect(movePlayField(playField2, DOWN)).toEqual(playField2Down);
 });
